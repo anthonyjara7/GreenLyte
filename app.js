50a01beed4f35f07d6949db83865c9d8b272eb1f
anthonyjara7 <90576643+anthonyjara7@users.noreply.github.com>
2023-01-07 10:48:39 -0600

    Updates app.js comments

--- app.js
+++ app.js
@@ -15,22 +15,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/socialApp')
 // Creates our Express application
 const app = express();
 
-app.set('view engine', 'ejs');  // Enables express to use ejs files for viewing
+app.set('view engine', 'ejs');  // Enables express to utilize ejs templates
 app.set('views', path.join(__dirname, 'views'));    // makes app automatically search the views folder
 
 app.use(express.urlencoded({ extended: true }));    // Enables express to parse html requests
-app.use(methodOverride('_method')); // Allows put and delete methods within ejs files
+app.use(methodOverride('_method')); // Allows put and delete endpoints through ejs templates
 app.use(express.static(path.join(__dirname, 'public')));    // Lets express serve files in the public directory, browser will reject files otherwise
 
 // Home route
 app.get('/', (req, res) => {
-    res.render('home'); // the view parameter should not begin with a '/' for res.render
+    res.render('home'); // the view should not begin with a '/' for res.render since res.render already looks in the views folder
 });
 
 // Displays all posts
 app.get('/posts', async (req, res) => {
     const posts = await Post.find({});
-    res.render('posts/index', { posts });
+    res.render('posts/index', { posts }); // passing in multiple posts as a single object
 })
 
 // Displays the create post form
@@ -40,7 +40,7 @@ app.get('/posts/new', async (req, res) => {
 
 // Creates a new post and submits it to the database
 app.post('/posts', async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     const post = new Post(req.body.post);
     await post.save();
     res.redirect(`/posts/${post._id}`);
